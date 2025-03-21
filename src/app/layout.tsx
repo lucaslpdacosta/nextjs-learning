3412236b6c5a99a4529aaf4a33402a656d6a401e
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-03-21 20:20:18 -0300

    feat: applying seo to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import "./globals.css";
 import { Header } from '../components/header';
- 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dominando Next.js',
+  description: 'Entendendo SEO no Next.js',
+  openGraph: {
+    title: 'Dominando Next.js do zero',
+    description: 'Entendendo SEO no Next.js'
+  },
+  robots:{
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
